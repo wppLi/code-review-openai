0fccdc653424d549d0edbc9ce9398cd0de919391
lijiaqiang <2148660566@qq.com>
2024-08-05 13:59:56 +0800

    to: 修复小bug

--- openai-code-review-sdk/src/main/java/cc/jq1024/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/cc/jq1024/middleware/sdk/OpenAiCodeReview.java
@@ -1,14 +1,10 @@
 package cc.jq1024.middleware.sdk;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class OpenAiCodeReview {
 
-    private static final Logger log = LoggerFactory.getLogger(OpenAiCodeReview.class);
 
     public static void main(String[] args) {
-        log.info("openai-code-review 测试开始");
+        System.out.println("openai-code-review 测试开始");
     }
 
 }
