2f816451be8e5407c6a3a1135cca0db18e32af55
lijiaqiang <2148660566@qq.com>
2024-11-19 14:55:22 +0800

    feat: 打包jar发布部署使用 v9.9

--- openai-code-review-sdk/src/test/java/cc/jq1024/middleware/AppTest.java
+++ openai-code-review-sdk/src/test/java/cc/jq1024/middleware/AppTest.java
@@ -1,165 +1,165 @@
-//package cc.jq1024.middleware;
-//
-//import cc.jq1024.middleware.sdk.infrastructure.openai.dto.ChatCompletionSyncResponseDTO;
-//import cc.jq1024.middleware.sdk.types.utils.BearerTokenUtils;
-//import com.alibaba.fastjson2.JSON;
-//import org.junit.Test;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.io.OutputStream;
-//import java.net.HttpURLConnection;
-//import java.net.URL;
-//import java.nio.charset.StandardCharsets;
-//import java.util.HashMap;
-//import java.util.Map;
-//import java.util.Scanner;
-//
-///**
-// * Unit test for simple App.
-// */
-//public class AppTest {
-//
-//    public static void main(String[] args) {
-//        String token = BearerTokenUtils.getToken();
-//        System.out.println(token);
-//    }
-//
-//
-//    @Test
-//    public void test_http() throws IOException {
-//        String token = BearerTokenUtils.getToken();
-//
-//        URL url = new URL("https://open.bigmodel.cn/api/paas/v4/chat/completions");
-//        // 开启连接
-//        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//
-//        connection.setRequestMethod("POST");
-//        connection.setRequestProperty("Authorization", "Bearer " + token);
-//        connection.setRequestProperty("Content-Type", "application/json");
-//        connection.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
-//        connection.setDoOutput(true);
-//
-//        String code = "1+1";
-//
-//        String jsonInpuString = "{"
-//                + "\"model\":\"glm-4-flash\","
-//                + "\"messages\": ["
-//                + "    {"
-//                + "        \"role\": \"user\","
-//                + "        \"content\": \"你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: " + code + "\""
-//                + "    }"
-//                + "]"
-//                + "}";
-//
-//
-//        try (OutputStream os = connection.getOutputStream()) {
-//            byte[] input = jsonInpuString.getBytes(StandardCharsets.UTF_8);
-//            os.write(input);
-//        }
-//
-//        int responseCode = connection.getResponseCode();
-//        System.out.println("请求状态码：" + responseCode);
-//
-//        // 读取结果
-//        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//        String inputLine;
-//        StringBuilder content = new StringBuilder();
-//        while ((inputLine = in.readLine()) != null) {
-//            content.append(inputLine);
-//        }
-//        // 关闭连接
-//        in.close();
-//        connection.disconnect();
-//
-//        // 转换结果
-//        ChatCompletionSyncResponseDTO response = JSON.parseObject(content.toString(), ChatCompletionSyncResponseDTO.class);
-//        System.out.println(response.getChoices().get(0).getMessage().getContent());
-//
-//    }
-//
-//    @Test
-//    public void test_wx() {
-////        String accessToken = WXAccessTokenUtils.getAccessToken();
-//        String accessToken = "83_O6jF21SVfUE4Trkv_MSg-WKVdXEE5lh3hdaWhTfrOBerG19wvfk6D5XH2HTkluVv5wL6H08X2vfqJapxEcEWNBnB84lGdA5pUAYXmLuAjivgiIeS7-P91E3u_bEPBWfADABNI";
-//        System.out.println(accessToken);
-//
-//        Message message = new Message();
-//        message.put("project","big-market");
-//        message.put("review","feat: 新加功能");
-//
-//        String url = String.format("https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=%s", accessToken);
-//        sendPostRequest(url, JSON.toJSONString(message));
-//    }
-//
-//    private static void sendPostRequest(String urlString, String jsonBody) {
-//        try {
-//            URL url = new URL(urlString);
-//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//            conn.setRequestMethod("POST");
-//            conn.setRequestProperty("Content-Type", "application/json; utf-8");
-//            conn.setRequestProperty("Accept", "application/json");
-//            conn.setDoOutput(true);
-//
-//            try (OutputStream os = conn.getOutputStream()) {
-//                byte[] input = jsonBody.getBytes(StandardCharsets.UTF_8);
-//                os.write(input, 0, input.length);
-//            }
-//
-//            try (Scanner scanner = new Scanner(conn.getInputStream(), StandardCharsets.UTF_8.name())) {
-//                String response = scanner.useDelimiter("\\A").next();
-//                System.out.println(response);
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public static class Message {
-//        private String touser = "oxd-l6lb0KOF5KRwBy7GDHxeXaeE";
-//        private String template_id = "HwS6_X9qNeOb2j2MWZZfg70xLiPdefcJMrV-xchIr8Y";
-//        private String url = "https://github.com/wppLi/opena-code-review-log/blob/main/2024-08-06/tusYcGvBphNE.md";
-//        private Map<String, Map<String, String>> data = new HashMap<>();
-//
-//        public void put(String key, String value) {
-//            data.put(key, new HashMap<String, String>() {
-//                {
-//                    put("value", value);
-//                }
-//            });
-//        }
-//
-//        public String getTouser() {
-//            return touser;
-//        }
-//
-//        public void setTouser(String touser) {
-//            this.touser = touser;
-//        }
-//
-//        public String getTemplate_id() {
-//            return template_id;
-//        }
-//
-//        public void setTemplate_id(String template_id) {
-//            this.template_id = template_id;
-//        }
-//
-//        public String getUrl() {
-//            return url;
-//        }
-//
-//        public void setUrl(String url) {
-//            this.url = url;
-//        }
-//
-//        public Map<String, Map<String, String>> getData() {
-//            return data;
-//        }
-//
-//        public void setData(Map<String, Map<String, String>> data) {
-//            this.data = data;
-//        }
-//    }
-//}
+package cc.jq1024.middleware;
+
+import cc.jq1024.middleware.sdk.infrastructure.openai.dto.ChatCompletionSyncResponseDTO;
+import cc.jq1024.middleware.sdk.types.utils.BearerTokenUtils;
+import com.alibaba.fastjson2.JSON;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    public static void main(String[] args) {
+        String token = BearerTokenUtils.getToken();
+        System.out.println(token);
+    }
+
+
+    @Test
+    public void test_http() throws IOException {
+        String token = BearerTokenUtils.getToken();
+
+        URL url = new URL("https://open.bigmodel.cn/api/paas/v4/chat/completions");
+        // 开启连接
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Authorization", "Bearer " + token);
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+        connection.setDoOutput(true);
+
+        String code = "1+1";
+
+        String jsonInpuString = "{"
+                + "\"model\":\"glm-4-flash\","
+                + "\"messages\": ["
+                + "    {"
+                + "        \"role\": \"user\","
+                + "        \"content\": \"你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: " + code + "\""
+                + "    }"
+                + "]"
+                + "}";
+
+
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = jsonInpuString.getBytes(StandardCharsets.UTF_8);
+            os.write(input);
+        }
+
+        int responseCode = connection.getResponseCode();
+        System.out.println("请求状态码：" + responseCode);
+
+        // 读取结果
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        // 关闭连接
+        in.close();
+        connection.disconnect();
+
+        // 转换结果
+        ChatCompletionSyncResponseDTO response = JSON.parseObject(content.toString(), ChatCompletionSyncResponseDTO.class);
+        System.out.println(response.getChoices().get(0).getMessage().getContent());
+
+    }
+
+    @Test
+    public void test_wx() {
+//        String accessToken = WXAccessTokenUtils.getAccessToken();
+        String accessToken = "83_O6jF21SVfUE4Trkv_MSg-WKVdXEE5lh3hdaWhTfrOBerG19wvfk6D5XH2HTkluVv5wL6H08X2vfqJapxEcEWNBnB84lGdA5pUAYXmLuAjivgiIeS7-P91E3u_bEPBWfADABNI";
+        System.out.println(accessToken);
+
+        Message message = new Message();
+        message.put("project","big-market");
+        message.put("review","feat: 新加功能");
+
+        String url = String.format("https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=%s", accessToken);
+        sendPostRequest(url, JSON.toJSONString(message));
+    }
+
+    private static void sendPostRequest(String urlString, String jsonBody) {
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json; utf-8");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setDoOutput(true);
+
+            try (OutputStream os = conn.getOutputStream()) {
+                byte[] input = jsonBody.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+
+            try (Scanner scanner = new Scanner(conn.getInputStream(), StandardCharsets.UTF_8.name())) {
+                String response = scanner.useDelimiter("\\A").next();
+                System.out.println(response);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class Message {
+        private String touser = "oxd-l6lb0KOF5KRwBy7GDHxeXaeE";
+        private String template_id = "HwS6_X9qNeOb2j2MWZZfg70xLiPdefcJMrV-xchIr8Y";
+        private String url = "https://github.com/wppLi/opena-code-review-log/blob/main/2024-08-06/tusYcGvBphNE.md";
+        private Map<String, Map<String, String>> data = new HashMap<>();
+
+        public void put(String key, String value) {
+            data.put(key, new HashMap<String, String>() {
+                {
+                    put("value", value);
+                }
+            });
+        }
+
+        public String getTouser() {
+            return touser;
+        }
+
+        public void setTouser(String touser) {
+            this.touser = touser;
+        }
+
+        public String getTemplate_id() {
+            return template_id;
+        }
+
+        public void setTemplate_id(String template_id) {
+            this.template_id = template_id;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public Map<String, Map<String, String>> getData() {
+            return data;
+        }
+
+        public void setData(Map<String, Map<String, String>> data) {
+            this.data = data;
+        }
+    }
+}
